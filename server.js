44cdee51d7336835c30aa6bcd08577d18ee59728
Git-admin <kailash.panigrahi@revoluxsolutions.com>
2019-02-01 19:13:17 +0530

    first commit firbase base push notification in Node.js

---
+++ server.js
@@ -0,0 +1,32 @@
+
+var FCM = require('fcm-node');
+
+var fcmServerKey = 'YOUR_FCM_SERVER_KEY';
+
+var fcm = new FCM(serverKey);
+
+var message = {
+    to: 'DEVICE_TOKEN_OF_DEVICE_WHICH_RECIVE NOTIFICATION',
+    
+    notification: {
+        title: 'Hello World',
+        body: 'This is body of push notification!!',
+    },
+
+   //this is optional where you can share media option  
+    data: {
+        "image": "http://Image-Link/icon.jpeg",
+        "message": "Firebase Push Message Using API",
+        "AnotherActivity": "True"
+    }
+};
+
+fcm.send(message, function (err, response) {
+    if (err) {
+        // error response
+        console.log(err);
+    } else {
+        // sucess response 
+        console.log("Successfully sent with response: ", response);
+    }
+});
